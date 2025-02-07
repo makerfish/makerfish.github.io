4211fdc1122855f1dc59bf9d3082490c70ff0fdd
makerfish <rileyfarberman@gmail.com>
2025-02-07 13:01:55 -0500

    contact form submission

--- contact.js
+++ contact.js
@@ -1,13 +1,34 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbzg7uY20fhJcRIMUsjf_LQXXBSs3w-hkTYDQdF2IKph6YGi1SMRkfHXv8NkR5D1I8yW/exec'
-
 const form = document.forms['contact-form']
+const container2 = document.querySelector('.container2')
 
-form.addEventListener('submit', e => {
-  
+form.addEventListener('submit', async e => {
   e.preventDefault()
-  
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("thanks for reaching out!" ))
-  .then(() => { window.location.reload(); })
-  .catch(error => console.error('Error!', error.message))
+  console.log('Form submitted') // Debugging log
+
+  // Provide immediate feedback to the user
+  container2.innerHTML = `
+    <h4>submitting your form...</h4>
+  `
+
+  try {
+    const response = await fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    console.log('Success!', response) // Debugging log
+    container2.innerHTML = `
+      <h4>your form has been submitted!</h4>
+      <button id="submit-another">submit another form</button>
+    `
+    document.getElementById('submit-another').addEventListener('click', () => {
+      window.location.reload()
+    })
+  } catch (error) {
+    console.error('Error!', error.message)
+    container2.innerHTML = `
+      <h4>there was an error submitting your form. please try again.</h4>
+      <button id="submit-another">try again</button>
+    `
+    document.getElementById('submit-another').addEventListener('click', () => {
+      window.location.reload()
+    })
+  }
 })
