2e8a28b6f16d750d4d228d62e7890b3d66364079
makerfish <rileyfarberman@gmail.com>
2025-02-14 11:22:26 -0500

    fix js 6

--- contact.js
+++ contact.js
@@ -1,27 +1,27 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbwCZbpZ1LDYl_5J0SqFGhKnjOJlC9gflWOWTFS_3rITMxgwszNmEuV6x0TUJgCUrsBrTg/exec';
 const form = document.forms['contact-form'];
 const submitButton = form.querySelector('button[type="submit"]');
-const feedbackMessage = document.createElement('p');
+const container2 = document.querySelector('.container2');
 
-form.appendChild(feedbackMessage); // Append the message element to the form
+form.appendChild(container2); // Append the message element to the form
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   // Disable the submit button and show a loading message
   submitButton.disabled = true;
-  feedbackMessage.textContent = 'submitting...';
+  container2.textContent = 'submitting...';
 
   fetch(scriptURL, { method: 'POST', body: new FormData(form) })
     .then((response) => {
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
-      feedbackMessage.textContent = 'thank you! your form has been submitted.';
+      container2.textContent = 'thank you! your form has been submitted.';
       form.reset();
     })
     .catch((error) => {
-      feedbackMessage.textContent = 'error submitting the form. please try again.';
+      container2.textContent = 'error submitting the form. please try again.';
       console.error('Error!', error.message);
     })
     .finally(() => {
